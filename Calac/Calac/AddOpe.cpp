e5a073a36c773421792319bed5637e1d2816b69f
dulaiming <dulaiming@gmail.com>
2019-06-14 17:19:48 +0900

    local modify commit;;;; since 2 month

--- Calac/Calac/AddOpe.cpp
+++ Calac/Calac/AddOpe.cpp
@@ -1,4 +1,4 @@
-//
+    //
 //  AddOpe.cpp
 //  Calac
 //
@@ -30,27 +30,71 @@ string AddOpe::Calc(string data1, string data2) {
     // calc per byte
     // rlt[0] = '0';
     char data = 0;
-    for(int i=maxSize-1; i>=0 ;i-- ){
-        data = 0;
-        char iData1 = pData1[i];
-        char iData2 = pData2[i];
+
+//    for(int i=maxSize-1; i>=0 ;i-- ){
+//        data = 0;
+//        char iData1 = pData1[i];
+//        char iData2 = pData2[i];
+//        
+//        if ((data2.length() < data1.length())) {
+//            i + minSize - maxSize < 0 ? iData2 = '0' : iData2 = pData2[i + minSize - maxSize];
+//            iData1 = pData1[i];
+//        } else {
+//            i + minSize - maxSize < 0 ? iData1 = '0' : iData1 = pData1[i + minSize - maxSize];
+//            iData2 = pData2[i];
+//        }
+////        (i >= data1.length()) ? iData1 = '0' : NULL;
+////        (i >= data2.length()) ? iData2 = '0' : NULL;
+//        data = (iData1-'0') + (iData2-'0') + rlt[i+1] -'0';
+//        char datamod = data % 10 + '0';
+ //       char dataInt = data / 10 + '0';
+  //      rlt[i+1] = datamod;
+   //     rlt[i] = dataInt;
+    
+    char carryDigit = '\0';
+    calcAdd(rlt, pData1, pData2, carryDigit);
+    
+    
+    char* carryL;
+    char* carryR;
+    
+    if ((data1.length() > data2.length())) {
+        // copy
+        memcpy(rlt, pData1, maxSize - minSize);
         
-        if ((data2.length() < data1.length())) {
-            i + minSize - maxSize < 0 ? iData2 = '0' : iData2 = pData2[i + minSize - maxSize];
-            iData1 = pData1[i];
-        } else {
-            i + minSize - maxSize < 0 ? iData1 = '0' : iData1 = pData1[i + minSize - maxSize];
-            iData2 = pData2[i];
-        }
-//        (i >= data1.length()) ? iData1 = '0' : NULL;
-//        (i >= data2.length()) ? iData2 = '0' : NULL;
-        data = (iData1-'0') + (iData2-'0') + rlt[i+1] -'0';
-        char datamod = data % 10 + '0';
-        char dataInt = data / 10 + '0';
-        rlt[i+1] = datamod;
-        rlt[i] = dataInt;
+        pData1 = pData1+(maxSize - minSize);
+    } else {
+        // copy
+        memcpy(rlt, pData2, maxSize - minSize);
         
+        pData2 = pData2+(maxSize - minSize);
     }
+    //
+    rlt = rlt+(maxSize - minSize);
+    
+    calcAdd(rlt, pData1, pData2, carryDigit);
+    
+    
+//    for(int i = maxSize - 1; i>=0 ;i-- ){
+//        data = 0;
+//        char iData1 = pData1[i];
+//        char iData2 = pData2[i];
+//
+//        if ((data2.length() < data1.length())) {
+//            i + minSize - maxSize < 0 ? iData2 = '0' : iData2 = pData2[i + minSize - maxSize];
+//            iData1 = pData1[i];
+//        } else {
+//            i + minSize - maxSize < 0 ? iData1 = '0' : iData1 = pData1[i + minSize - maxSize];
+//            iData2 = pData2[i];
+//        }
+////        (i >= data1.length()) ? iData1 = '0' : NULL;
+////        (i >= data2.length()) ? iData2 = '0' : NULL;
+//        data = (iData1-'0') + (iData2-'0') + rlt[i+1] -'0';
+//        char datamod = data % 10 + '0';
+//        char dataInt = data / 10 + '0';
+//        rlt[i+1] = datamod;
+//        rlt[i] = dataInt;
+//    }
     
     if (rlt[0] == 0) {
         rlt[0] = '0';
--- Calac/h/AddOpe.hpp
+++ Calac/h/AddOpe.hpp
@@ -21,6 +21,44 @@ public:
     ~AddOpe(){};
     
     string Calc(string , string );
+    
+    template<typename T, typename ... Types> bool calcAddVariadic(T* pRlt, Types* ... pDatas) {
+        
+        cout<<sizeof...(Types);
+        return true;
+    }
+    
+    template<typename T> void calcAdd(T* pRlt, const T* data1, const T* data2, T carryDigit) {
+        
+        // end condition : data1 and data2 both '/0'
+
+        // ALL
+        const char *pAllRlt;
+        // calc
+        *pRlt = *data1 + *data2;
+        
+        // count
+        char datamod = *pRlt % 10 + '0';
+        char dataInt = *pRlt / 10 + '0';
+        
+        addCarryDigit(pRlt--, dataInt);
+        
+        *pRlt = datamod;
+        
+        calcAdd(pRlt++, data1++, data2++, carryDigit);
+        
+    }
+    
+    template<typename T> void addCarryDigit(T* pRlt, T carryDigit){
+        *pRlt = *pRlt + carryDigit;
+        char datamod = *pRlt % 10 + '0';
+        char dataInt = *pRlt / 10 + '0';
+        *pRlt = datamod;
+        //
+        if (dataInt > '0') {
+            addCarryDigit(pRlt--, dataInt);
+        }
+    }
 };
 
 #endif /* AddOpe_hpp */
